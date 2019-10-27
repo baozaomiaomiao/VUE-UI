10821093b3853db2a8fd514bef443e57793a91f9
baozaomiaomiao <liyang980209@163.com>
2019-10-27 20:09:06 +0800

    init

---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<template>
+	<button class="g-button">按钮</button>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.g-button {
+	font-size: var(--font-size);
+	height: var(--button-height);
+	padding: 0 1em;
+	border-radius: var(--border-radius);
+	border: 1px solid var(--border-color);
+	background: var(--button-bg);
+	&:hover {
+		border-color: var(--border-color-hover);
+	}
+	&:active {
+		background-color: var(--button-active-bg);
+	}
+	&:focus {
+		outline: none; /*去除CSS控制按钮，导致蓝色边框的问题*/
+	}
+}
+</style>
