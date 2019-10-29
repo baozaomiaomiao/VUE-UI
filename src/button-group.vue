524b4edc81af80e706a43105120e2b4ade890fef
baozaomiaomiao <liyang980209@163.com>
2019-10-29 13:00:14 +0800

    解决 button group 的bug

--- src/button-group.vue
+++ src/button-group.vue
@@ -5,7 +5,17 @@
 </template>
 
 <script>
-export default {};
+export default {
+	// 处理边框BUG，当子元素不是g-button 时，发出警告
+	mounted() {
+		for (let node of this.$el.children) {
+			let name = node.nodeName.toLowerCase();
+			if (name !== 'button') {
+				console.warn(`g-button-group 的子元素只能是 g-button, 请去除 ${name}`);
+			}
+		}
+	}
+};
 </script>
 
 <style lang="scss">
@@ -14,7 +24,9 @@ export default {};
 	vertical-align: middle;
 	> .g-button {
 		border-radius: 0;
-		margin-left: -1px; //解决边框不合并的问题,新bug边的按钮被覆盖掉一个边框
+		&:not(:first-child) {
+			margin-left: -1px; //解决边框不合并的问题,新bug边的按钮被覆盖掉一个边框
+		}
 		&:first-child {
 			border-top-left-radius: var(--border-radius);
 			border-bottom-left-radius: var(--border-radius);
